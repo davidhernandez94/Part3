71d391675b4e8c3afc8d13e2db5c8ee0be50b872
davidhernandez94 <david.hernandez.011011@gmail.com>
2024-11-15 13:38:00 -0500

    third commit nov 15 2024

--- src/CompactDisc.java
+++ src/CompactDisc.java
@@ -8,12 +8,13 @@ public class CompactDisc
 {
     public static void main(String[] args)throws IOException
     {
-        Scanner input = new Scanner(new File("Classics.txt"));
+        Scanner input = new Scanner(new File("C:\\Users\\6309110\\IdeaProjects\\Part3\\src\\Classics.txt"));
         String title;
         String artist;
 // ADD LINES FOR TASK #3 HERE
-// Declare an array of Song objects, called cd, // with
-        a size of 6
+// Declare an array of Song objects, called cd,
+// with a size of 6
+        Song[] cd = new Song[6];
         for (int i = 0; i < cd.length; i++)
         {
             title = input.nextLine();
@@ -22,12 +23,14 @@ public class CompactDisc
 // Fill the array by creating a new song with
 // the title and artist and storing it in the
 // appropriate position in the array
+            cd[i] = new Song(title, artist);
         }
         System.out.println("Contents of Classics:");
         for (int i = 0; i < cd.length; i++)
         {
 // ADD LINES FOR TASK #3 HERE
 // Print the contents of the array to the console
+            System.out.print(cd[i].toString());
         }
     }
 }
